eccf7e4f1423d88456f940cff508de30ea71d550
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2019-10-30 09:24:07 +0100

    step 1 script

--- convert.sh
+++ convert.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+mvn package
+mv target/round2-0.0.1-SNAPSHOT-full.jar code.jar
+
+ODIR=/Users/reiterns/Documents/Projects/SharedTasks/phase-1-round-2-test-corpus
+
+
+# step 1: Convert all files from UIMA XMI to CSV
+for S in SANTA1 SANTA2 SANTA4 SANTA5 SANTA6 SANTA7 SANTA8
+do
+	for i in $(ls $ODIR/$S)
+	do
+		mkdir -p target/d/$S
+		java -Djava.awt.headless=true -cp code.jar de.unistuttgart.ims.creta.santa.round2.GenerateCSV --input $ODIR/$S/$i --outputDirectory target/d/$S
+	done
+done
+
